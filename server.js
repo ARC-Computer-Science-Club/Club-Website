c365c979e9c910a32fe81b5d85f77b5501854c8d
Devin Scholl <DevinScholl1@gmail.com>
2018-11-04 18:59:41 -0800

    Final tweaks before the website goes live

--- server.js
+++ server.js
@@ -1,47 +1,29 @@
 const express = require('express');
-const HTTP = require('http');
-const URL = require('url').URL;
-const PATH = require('path');
-const FS = require('fs');
+const path = require('path');
 
 const app = express();
-const port = 8080;
-const ROOT = PATH.join(process.cwd(), "/build");
+const port = process.env.PORT || 8080;
 
-app.get('/*', (req, res) => {
-    var path = new URL(req.url, "http://localhost").pathname;
-    console.log("%s | %s | %s | %s | %s", req.headers.host, new Date().toString(), req.method, path, req.headers['user-agent']);
 
-    var filepath = PATH.join(ROOT, path);
+// helper function
+var static_middleware = (path_to_static_files) => {
+    return express.static(path.join(__dirname, path_to_static_files));
+};
 
-    FS.readFile(filepath, (err, file_data) => {
-        if (err) {
-            res.set('Content-Type', 'text/plain');
-            res.status(404).send(HTTP.STATUS_CODES[404]);
-        }
-        else {
-            var ext = PATH.extname(filepath);
-            if (ext == ".html") {
-                res.set('Content-Type', 'text/html');
-            }
-            else if (ext == ".css") {
-                res.set('Content-Type', 'text/css');
-            }
-            else if (ext == ".js") {
-                res.set('Content-Type', 'text/js');
-            }
-            else if (ext == ".png") {
-                res.set('Content-Type', 'image/png');
-            }
-            else if (ext == ".jpg") {
-                res.set('Content-Type', 'image/jpg');
-            }
-            else {
-                res.set('Content-Type', 'text/plain');
-            }
-            res.status(200).send(file_data);
-      }
-    });
+
+app.use((req, res, next) => {
+    console.log("%s | %s | %s | %s | %s", req.headers.host, new Date().toString(), req.method, req.url, req.headers['user-agent']);
+	next();
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+app.get('/', (req, res) => {
+	res.redirect('/html/home_page.ejs.html');
+});
+
+
+// static assets
+app.use('/', static_middleware('build'));
+
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
